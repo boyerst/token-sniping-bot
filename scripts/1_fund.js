5372362d3fd7072aa2f9f3a2655a3c902f0b7551
Steve Boyer <stephen.a.boyer@gmail.com>
2022-02-23 12:50:57 -0600

    Prepare app for LP deployment

--- scripts/1_fund.js
+++ scripts/1_fund.js
@@ -1,19 +1,36 @@
+// This script will
+  // Fund the deployer account to create a new pool of WETH/IERC20
+  // Fund the sniper bot that will buy tokens with WETH
+
+// Grab our token contract (the interface for our basic ERC20 token from openzeppelin)
 const IERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json')
+
+// Grab WETH contract to use as pair for our Uniswap pool
 const WETH = new web3.eth.Contract(IERC20.abi, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
 
+// An account from the mainnet that has WETH in it for us to use
+  // We are pretending to be this account
 const UNLOCKED_ACCOUNT = '0x2fEb1512183545f48f6b9C5b4EbfCaF49CfCa6F3'
 
 module.exports = async function (callback) {
+    // Deployer is one of our accounts on ganache that runs the 'Uniswap LP'
+      // We fund it with WETH and our ERC20 token
+    // Sniper is the trading bot that will buy the token in the 'Uniswap LP'
+      // We buy the tokens with WITH
     const [deployer, sniper] = await web3.eth.getAccounts()
 
+    // We declare an amount...
     const amount = web3.utils.toWei('10', 'ether')
-
+    // ...to transfer from unlocked account to deployer...
     await WETH.methods.transfer(deployer, amount).send({ from: UNLOCKED_ACCOUNT })
+    // .. and also from the unlocked account to the sniper
     await WETH.methods.transfer(sniper, amount).send({ from: UNLOCKED_ACCOUNT })
 
+    // Check bal of pool
     const deployerBalance = await WETH.methods.balanceOf(deployer).call()
     console.log(`WETH amount in deployer: ${deployerBalance / 1e18}\n`)
 
+    // Check bal of sniper
     const sniperBalance = await WETH.methods.balanceOf(sniper).call()
     console.log(`WETH amount in sniper: ${sniperBalance / 1e18}\n`)
 
--- scripts/2_create_pool.js
+++ scripts/2_create_pool.js
@@ -1,41 +1,64 @@
+// Fetches our token artifact 
 const Token = artifacts.require("Token")
+// Fetch the Uniswap Router interface
 const IUniswapV2Router02 = require('@uniswap/v2-periphery/build/IUniswapV2Router02.json')
+// Fetch the IERC20 artifact
 const IERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json')
 
+// Create a new version of Uniswap router with the actual mainnet address
+  // This is the JS version created with the router artifact declared above
 const uRouter = new web3.eth.Contract(IUniswapV2Router02.abi, '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
 
 module.exports = async function (callback) {
     console.log(`Preparing to create Uniswap pool...\n`)
 
+    // Declare deployer as the logged in user?
     const [deployer] = await web3.eth.getAccounts()
 
+    // Declare and fetch the WETH contract we are forking to our local env
     const WETH = new web3.eth.Contract(IERC20.abi, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
+    // Declare and fetch our token we declared in this project
     const DAPPU = await Token.deployed()
 
+    // Declare an equal ratio of tokens
+      // Where 250 tokens = 1 WETH
+      // Denominated in ether but this really just means 'tokens'
+      // Both DAPP and WETH have 18 decimals places
     const DAPPUAmount = web3.utils.toWei('250', 'ether')
     const WETHAmount = web3.utils.toWei('1', 'ether')
 
     console.log(`Approving WETH...`)
 
+    // Approve the WETH so Uniswap can spend it on your behalf
     await WETH.methods.approve(uRouter._address, WETHAmount).send({ from: deployer })
 
     console.log(`Approving DAPPU...\n`)
-
+    // Approve DAPP contract so Uniswap can spend it on your behalf
     await DAPPU.approve(uRouter._address, DAPPUAmount, { from: deployer })
 
     console.log(`Creating Uniswap pool...\n`)
 
+    // Estimate gas so can calculate proper amount for the addLiquidity tx
     const gas = await uRouter.methods.addLiquidity(
+        // address pool TokenA 
         DAPPU.address,
+        // address pool TokenB
         WETH._address,
+        // The amount of tokenA to add as liquidity if the B/A price is <= amountBDesired/amountADesired (A depreciates)
         DAPPUAmount,
+        // The amount of tokenB to add as liquidity if the A/B price is <= amountADesired/amountBDesired (B depreciates)
         WETHAmount,
+        // uint amountAMin
         DAPPUAmount,
+        // unit amountBMin
         WETHAmount,
+        // Deployer address
         deployer,
+        // Deadline (Unix timestamp after which the transaction will revert)
         Math.floor(Date.now() / 1000) + 60 * 10
     ).estimateGas({ from: deployer })
 
+    // Add liquidity to the pool
     await uRouter.methods.addLiquidity(
         DAPPU.address,
         WETH._address,
@@ -45,9 +68,13 @@ module.exports = async function (callback) {
         WETHAmount,
         deployer,
         Math.floor(Date.now() / 1000) + 60 * 10
+    // Gas = gas amount that was estimated above
     ).send({ from: deployer, gas: gas })
 
     console.log(`Pool successfully created!\n`)
 
     callback()
 }
+
+
+
